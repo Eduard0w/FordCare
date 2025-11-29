4285ba08ac39c85e2517850d999024082ad3b2eb
Eduard0w <dudusouza5595@gmail.com>
2025-11-28 23:08:25 -0300

    Avanço na tela home
    criação de novas sections e carrosel implementado na section principal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'azul-texto': '#234BFF',
+        'azul-icon': '#0014A8',
+      },
+    },
   },
   plugins: [],
-}
-
+};
