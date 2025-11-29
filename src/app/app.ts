aab24be1732906dec5d33b3e7a4c9f4bc8d4d13f
Eduard0w <dudusouza5595@gmail.com>
2025-11-29 13:57:44 -0300

    tela Home 90% concluida
    foram acrescentado todas as sections necessárias, além de icones e animações

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './pages/home/home';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,16 @@ import { Home } from './pages/home/home';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
+  ngOnInit(): void {
+    try {
+      AOS.init({
+        duration: 2000, // Duração da animação em milissegundos (1s)
+        once: true, // Se 'true', anima só na primeira vez que desce a tela
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
   protected readonly title = signal('FordCare');
 }
