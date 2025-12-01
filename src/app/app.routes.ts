f74459ae7e3fc1483ce4c374be1bc8df9277e606
Eduard0w <dudusouza5595@gmail.com>
2025-12-01 09:19:34 -0300

    Tela de veiculos adicionada, mas sem ser definada ainda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { LoginComponent } from './pages/login/login.component';
 import { CreatAcountComponent } from './pages/creat-acount/creat-acount.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CreatVehicleComponent } from './pages/creat-vehicle/creat-vehicle.component';
+import { VeiculosComponent } from './pages/veiculos/veiculos.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Rota coringa para URLs não encontradas
   { path: 'login', component: LoginComponent },
   { path: 'creatAcount', component: CreatAcountComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'vehicle', component: CreatVehicleComponent },
+  { path: 'vehicle', component: VeiculosComponent },
+  { path: 'creatVehicle', component: CreatVehicleComponent },
   { path: '**', component: HomeComponent },
 ];
 
